64c0be8576a5318265dde3db749e4540684f3bb1
Arthur Augis <92651106+ArthurAugis@users.noreply.github.com>
2025-02-14 15:11:08 +0100

    Update +Page.tsx

--- pages/index/+Page.tsx
+++ pages/index/+Page.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "../../components/Link";
-import pokeball from '../../assets/pokeball.ico';
+import pokeball from '../../assets/Pokeball.ico';
 import noname from '../../assets/noname.jpg';
 
 interface Pokemon {
